9e1bde2614be62c3ffa4333d8ac586e5f3243611
Erwann Charlier <69734768+ErwannCharlier@users.noreply.github.com>
2023-10-20 16:39:22 +0200

    er

---
+++ Models/Sudoku.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOn.Models
+{
+    public class Sudoku
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Grid { get; set; }
+        public string SolutionGrid { get; set; }
+        public bool IsRanked { get; set; }
+        public int Difficulty { get; set; }
+
+        
+        public Sudoku(int id, DateTime creationDate, string grid, string solutionGrid, bool isRanked, int difficulty)
+        {
+            Id = id;
+            CreationDate = creationDate;
+            Grid = grid;
+            SolutionGrid = solutionGrid;
+            IsRanked = isRanked;
+            Difficulty = difficulty;
+        }
+
+        
+    }
+
+}
--- Models/SudokuParticipation.cs
+++ Models/SudokuParticipation.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GameOn.Models
 {
-    public class GameParticipation
+    public class SudokuParticipation
     {
     }
 }
